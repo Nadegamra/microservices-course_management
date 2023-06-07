68d6560d6c7c1fe6ccbfd7bd365c00acfea15745
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-06-07 18:13:03 +0300

    Added integration events handling for UserName/User Email change

---
+++ IntegrationEvents/Events/UserNameChangedIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.EventBus.Generic.IntegrationEvents;
+
+namespace CourseManagement.IntegrationEvents.Events
+{
+    public class UserNameChangedIntegrationEvent : IntegrationEvent
+    {
+        public required int UserId { get; set; }
+        public required string OldUserName { get; set; }
+        public required string NewUserName { get; set; }
+    }
+}
